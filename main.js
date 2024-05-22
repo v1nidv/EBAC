6241ae54e9868b22ead79d07a56aab347ec1d38c
Vinicius de Moraes <d1moraes03vini@gmail.com>
2024-05-21 23:48:14 -0300

    js ok

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const valorX = document.getElementById('numberX');
-const valorY = documebt.getElementById('numberY');
+const valorY = document.getElementById('numberY');
 const form = document.getElementById('form-calcular');
 
 function calcular(numero){
-    numero = valorX.value > valorY.value;
+    numero = parseInt(valorX.value) > parseInt(valorY.value);
     return numero;
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const mensagemSucesso = `O número &nbsp;${valorX.value} &nbsp; é maior que o número &nbsp; ${valorY.value}`;
-    const mensagemErro = `O número &nbsp;${valorX.value}&nbsp; é menor que o número &nbsp; ${valorY.value};`
+    const mensagemSucesso = `O número &nbsp;<b>${valorX.value}<b> &nbsp; é maior que o número &nbsp; <b>${valorY.value}<b>`;
+    const mensagemErro = `O número &nbsp;<b>${valorX.value}<b>&nbsp; é menor que o número &nbsp; <b>${valorY.value}<b>`;
     if (calcular()){
-        
+        document.querySelector('#mensagem').innerHTML = mensagemSucesso;
+        document.querySelector('#mensagem').classList.add('sucess-message');
+        document.querySelector('#mensagem').classList.remove('errorMessage');
+        document.querySelector('#mensagem').style.display = 'flex';
+    } else {
+        document.querySelector('#mensagem').innerHTML = mensagemErro;
+        document.querySelector('#mensagem').style.display = 'flex';
+        document.querySelector('#mensagem').classList.remove('sucess-message');
+        document.querySelector('#mensagem').classList.add('errorMessage');
     }
 })
-
-console.log(form);
